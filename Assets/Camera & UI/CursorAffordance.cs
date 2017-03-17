feffd7fccb121c662e2c0221b06405e5a95ac1d3
Balazs Kozma <kozma.balazs1992@gmail.com>
2017-03-17 17:33:00 +0100

    switch between movement types

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CameraRaycaster))]
+public class CursorAffordance : MonoBehaviour {
+
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D attackCursor = null;
+	[SerializeField] Texture2D nullCursor = null;
+	[SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+	CameraRaycaster cameraRaycaster;
+
+	void Start () {
+		cameraRaycaster = GetComponent<CameraRaycaster> ();
+	}
+	
+	void LateUpdate () {
+		switch (cameraRaycaster.currentLayerHit) {
+		case Layer.Walkable:
+			Cursor.SetCursor (walkCursor, cursorHotspot, CursorMode.Auto);
+			break;
+		case Layer.Enemy:
+			Cursor.SetCursor (attackCursor, cursorHotspot, CursorMode.Auto);
+			break;
+		case Layer.RaycastEndStop:
+			Cursor.SetCursor (nullCursor, cursorHotspot, CursorMode.Auto);
+			break;
+		default:
+			Debug.LogError ("Don't know what cursor to show.");
+			return;
+		}
+	}
+}
